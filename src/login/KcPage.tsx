80ffcb42b8204b5f88e48fc4047d45c3dbe868dc
rtem <fstarhurma@yandex.ru>
2025-02-05 00:32:40 +0500

    add_login_page

--- src/login/KcPage.tsx
+++ src/login/KcPage.tsx
@@ -1,34 +1,46 @@
+import type {} from "react";
 import { Suspense, lazy } from "react";
 import type { ClassKey } from "keycloakify/login";
 import type { KcContext } from "./KcContext";
 import { useI18n } from "./i18n";
 import DefaultPage from "keycloakify/login/DefaultPage";
 import Template from "keycloakify/login/Template";
+import "./main.css";
+
 const UserProfileFormFields = lazy(
     () => import("keycloakify/login/UserProfileFormFields")
 );
-
+const Login = lazy(() => import("./pages/Login"));
 const doMakeUserConfirmPassword = true;
 
 export default function KcPage(props: { kcContext: KcContext }) {
     const { kcContext } = props;
-
     const { i18n } = useI18n({ kcContext });
 
     return (
-        <Suspense>
+        <Suspense fallback={null}>
             {(() => {
                 switch (kcContext.pageId) {
+                    case "login.ftl":
+                        return (
+                            <Login
+                                {...{ kcContext, i18n, classes }}
+                                Template={Template}
+                                doUseDefaultCss={true}
+                                htmlClass={classes.kcHtmlClass}
+                                bodyClass={classes.kcBodyClass}
+                            />
+                        );
                     default:
                         return (
                             <DefaultPage
-                                kcContext={kcContext}
-                                i18n={i18n}
-                                classes={classes}
+                                {...{ kcContext, i18n, classes }}
                                 Template={Template}
                                 doUseDefaultCss={true}
                                 UserProfileFormFields={UserProfileFormFields}
                                 doMakeUserConfirmPassword={doMakeUserConfirmPassword}
+                                htmlClass={classes.kcHtmlClass}
+                                bodyClass={classes.kcBodyClass}
                             />
                         );
                 }
@@ -37,4 +49,7 @@ export default function KcPage(props: { kcContext: KcContext }) {
     );
 }
 
-const classes = {} satisfies { [key in ClassKey]?: string };
+const classes = {
+    kcHtmlClass: "akzhol-pattern",
+    kcBodyClass: "akzhol-background"
+} satisfies { [key in ClassKey]?: string };
